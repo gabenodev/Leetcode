3328a370c532f6c15b7c4ac8846ea9a34506ad26
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2021-10-04 16:42:11 +0300

    Time: 3 ms (51.05%), Space: 63.1 MB (33.73%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -14,3 +14,31 @@ class Solution {
         
     }
 }
+
+/*
+i= 0;
+0 > 0 false
+
+index_value= Math.max(0 + 2 , 0) => index_value = 2;
+
+
+i=1;
+1 > 2 false
+index_value = Math.max(1 + 3 , 2) => index_value = 4;
+
+i=2;
+2 > 4 false
+index_value = Math.max(2+1 , 4) => index_value = 4;
+
+i=3;
+3>4 false
+index_value = Math.max(3+1 , 4) => index_value = 4;
+
+i=4
+4>4 false
+index_value = Math.max(4+4, 4 ) = > index_value = 8;
+
+return true;
+
+
+*/
