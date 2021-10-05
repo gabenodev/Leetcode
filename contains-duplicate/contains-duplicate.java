34b6fb7c9c9decf4ad9dbb4bee6fb7afdfd98b21
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2021-10-05 08:26:09 +0300

    Update contains-duplicate.java
    Runtime: 7 ms, faster than 41.93% of Java online submissions for Contains Duplicate.
    Memory Usage: 47 MB, less than 39.87% of Java online submissions for Contains Duplicate.

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         
+        
+        /* Simple hashmap solution to see if there is a same key in hashmap
+        
+            Runs a bit slower
+            
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
         
         for(int i = 0 ; i < nums.length; i++){
@@ -10,6 +15,22 @@ class Solution {
                 map.put(nums[i],i);
         }
         
+        return false;
+        
+        */
+        
+        
+        //Iterative solution
+        // Faster solution
+        
+        Arrays.sort(nums);
+        
+        for(int i = 1 ; i < nums.length; i++){
+        
+        if(nums[i] == nums[i-1]){
+            return true;
+        }
+    }
         return false;
     }
 }
