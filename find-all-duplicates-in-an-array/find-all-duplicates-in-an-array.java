0e4d5595644bcdb73139a8faf9339dd67469c98f
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2021-10-06 16:37:27 +0300

    Time: 15 ms (26.15%), Space: 47.9 MB (80.56%) - LeetHub

--- find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,6 +1,9 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         
+        
+        // The time complexity is not so friendly :)
+        
         Arrays.sort(nums);
         List<Integer> duplicates = new ArrayList<>();
         
@@ -11,5 +14,27 @@ class Solution {
         }
         
         return duplicates;
+        
+        
+        
+        
+        /* Time limit exceeds + space complexity exceeds
+        List<Integer> duplicates = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        
+        for(int i = 0 ; i < nums.length ; i++){
+           int seen = nums[i];
+            if(map.containsValue(seen)){
+                duplicates.add(nums[i]);
+            } else
+                map.put(i,nums[i]);
+        }
+        
+        return duplicates;
+        
+        */
+        
+        
+        
     }
 }
