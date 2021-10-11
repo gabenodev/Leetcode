09b940d59342371226784e17dc9b8158823aac10
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2021-10-11 13:02:59 +0300

    Update diameter-of-binary-tree.java
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Diameter of Binary Tree.
    Memory Usage: 40.3 MB, less than 32.29% of Java online submissions for Diameter of Binary Tree.

--- diameter-of-binary-tree/diameter-of-binary-tree.java
+++ diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -15,6 +15,34 @@
  */
 class Solution {
     
+    // DFS implementation 
+    // Faster than 100% submissions
+    int ans = 1;
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        dfs(root);
+        return ans-1;
+        
+    }
+    
+    public int dfs(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        
+        ans = Math.max(left+right+1,ans);
+        
+        return Math.max(left,right)+1;
+    }
+    
+    
+}
+
+    /*
     int diameter = 0;
     
     public int maxHeight(TreeNode root){
@@ -33,5 +61,8 @@ class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         maxHeight(root);
         return diameter;
+    }
+}
+*/
     }
 }
