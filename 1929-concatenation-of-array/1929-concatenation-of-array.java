9109b75b6439ebcede25288d0a16cf129fed9746
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2022-02-27 23:31:04 +0200

    Time: 0 ms (100.00%), Space: 42.7 MB (83.72%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -4,6 +4,16 @@ class Solution {
         int ans[] = new int[nums.length*2];
         int size = nums.length;
         
+        
+        // 1 for loop
+        
+        for(int i = 0 ; i < nums.length; i++){
+            ans[i] = nums[i];
+            ans[size+i] = nums[i];
+            
+        }
+        
+        /*
         for(int i = 0 ; i < nums.length; i++){
             ans[i] = nums[i];
         }
@@ -12,6 +22,7 @@ class Solution {
             ans[size] = nums[i];
             size++;
         }
+        */
         
         return ans;
         
