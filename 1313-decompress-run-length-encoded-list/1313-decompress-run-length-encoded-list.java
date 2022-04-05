76b9915cf7eda65821045812521d09b2ccc49551
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2022-04-05 12:36:18 +0300

    Time: 5 ms (39.45%), Space: 49.5 MB (6.02%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        
+        List<Integer> arr = new ArrayList<>();
+        
+       for(int i = 0 ; i < nums.length ; i+=2){
+           int freq = nums[i];
+           int val = nums[i+1];
+           
+           int j = 0 ;
+           
+           while(j < freq){
+               arr.add(val);
+               j++;
+           }
+       }
+        
+         int[] res = new int[arr.size()];
+        for(int index = 0 ; index < arr.size(); index++){
+            res[index] = arr.get(index);
+        }
+        
+        return res;
+        
+    }
+}
