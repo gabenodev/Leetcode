59e1fd14bbea4ab64f0b5b735d6d46591efd145f
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2022-04-05 14:37:57 +0300

    Time: 206 ms (5.44%), Space: 48.5 MB (83.99%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.js
+++ 1720-decode-xored-array/1720-decode-xored-array.js
@@ -6,12 +6,17 @@
 var decode = function(encoded, first) {
     
     
-    var numbers = []
-    numbers[0] = first
+    var numbers = [first]
     
+    
+    for(let i = 0 ; i < encoded.length ; i++){
+        numbers.push(encoded[i] ^ numbers[numbers.length-1])
+    }
+    /*
     for(let i = 0 ; i < encoded.length ; i++){
         numbers[i+1] = encoded[i] ^ numbers[i]
     }
+    */
     
     return numbers
     
