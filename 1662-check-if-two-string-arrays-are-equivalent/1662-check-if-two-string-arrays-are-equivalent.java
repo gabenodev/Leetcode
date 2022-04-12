2ed9c30a868f3990ba4d886d480ed6a1d7e5987d
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2022-04-12 12:51:07 +0300

    Time: 2 ms (51.21%), Space: 42.5 MB (20.64%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         
-     return(String.join("", word1).equals(String.join("", word2)));   
-        
-    }
-        
-        /* 
+    // return(String.join("", word1).equals(String.join("", word2)));   
+             
             String res1 ="";
             String res2 = "";
             
         
-        for(int i = 0 ; i < biggerSize(word1,word2); i++){
-            
+        for(int i = 0 ; i < word1.length; i++){
             res1 += word1[i];
+        }
+        
+        for(int i = 0 ; i < word2.length; i++){
             res2 += word2[i];
-            
         }
             
             if(res1.equals(res2)) return true;
@@ -28,5 +26,5 @@ class Solution {
         if(word1.length >= word2.length) return word1.length;
         return word2.length;
     }
-    */
+    
 }
