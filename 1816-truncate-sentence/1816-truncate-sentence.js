cad0353024dbd20affc1cc40cdc6ed56b0245c30
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2022-04-13 14:55:24 +0300

    Time: 57 ms (92.90%), Space: 42.3 MB (33.40%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.js
+++ 1816-truncate-sentence/1816-truncate-sentence.js
@@ -7,8 +7,9 @@ var truncateSentence = function(s, k)  {
     
     let spaces = 0;
     for(let i = 0 ; i < s.length; i ++){
-        if(s[i] === ' ' && ++spaces === k){
-            return s.slice(0,i)
+        if(s[i] === ' '){
+            spaces ++
+            if(spaces === k) return s.slice(0,i)
         }
     }
     
