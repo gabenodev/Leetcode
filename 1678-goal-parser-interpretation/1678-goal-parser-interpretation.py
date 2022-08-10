1835a69c31f6ed177bc00f63daff489d3c69197f
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2022-08-10 11:45:30 +0300

    Time: 25 ms (58.70%), Space: 13.6 MB (30.21%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,6 +1,31 @@
 class Solution(object):
     def interpret(self, command):
-        return command.replace('()' , 'o').replace('(al)','al')
+        
+    #    return command.replace('()' , 'o').replace('(al)','al')
+    
+        string = ''
+        i = 0
+    
+        while( i < len(command)):
+        
+            if(command[i] == 'G'):
+                string += 'G'
+                i += 1
+            
+            elif(command[i] == '(' and command[i+1] == 'a'):
+                string += 'al'
+                i += 4
+            
+            else:
+                string += 'o'
+                i += 2
+            
+            
+    
+        return string
+        
+    
+    
         """
         :type command: str
         :rtype: str
