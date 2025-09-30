5f50e3a5d5450cc54056415559cc2b927d2087ab
Gabriel Petcu <50408547+gabenodev@users.noreply.github.com>
2025-09-30 11:53:28 +0300

    Time: 44 ms (88%), Space: 58.5 MB (88%) - LeetHub

---
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.ts
@@ -0,0 +1,31 @@
+// 2 solutions
+// 1st solution -> (intuitive for me) recursion -> cleaner code but not optimal on big arrays
+// 2nd solution -> while-> we do it in one operation instead of recalling the function
+
+/*
+function triangularSum(nums: number[]): number {
+    if (nums.length == 1) {
+        return nums[nums.length-1];
+    }
+    let newNums: number[] = [];
+    for(let i = 0 ; i < nums.length-1; i++){
+        newNums[i] = ( nums[i] + nums[i+1] ) % 10;
+    }
+    return triangularSum(newNums)
+};
+ */
+
+ function triangularSum(nums: number[]): number {
+
+    if(nums.length == 1){
+        return nums[nums.length-1];
+    }
+
+    while(nums.length != 1){
+        for(let i = 0 ; i < nums.length-1; i++){
+            nums[i] = (nums[i] + nums[i+1]) % 10;
+        }
+        nums.pop();
+    }
+    return nums[0];
+ }
